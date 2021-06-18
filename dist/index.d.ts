d2fe2a5d098f458444d6ab297e51a6beaa740269
Jabert SI CHAIB <J.sichaib@live.fr>
2021-06-18 17:41:16 +0200

    Delete dist directory (#6)
    Add dist into ignore files
    
    Co-authored-by: Jabert <jabert.sichaib@socotec.com>

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Components } from "./interfaces/Components";
-import { GlobalConfig } from "./interfaces/Config";
-declare const _default: {
-    install(components: Components, options: GlobalConfig): void;
-};
-export default _default;
--- dist/mixins/Model.d.ts
+++
@@ -1,4 +0,0 @@
-import { Model as BaseModel } from "@vuex-orm/core";
-import { Model } from "../interfaces/Model";
-import { GlobalConfig } from "../interfaces/Config";
-export declare function ModelMixin(model: Model & typeof BaseModel, config: GlobalConfig): void;
